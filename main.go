731d39d34f88f2de21d0e717c59d884f65af7c52
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-01-10 21:01:17 -0800

    Making more progress on the prototype

--- main.go
+++ main.go
@@ -5,21 +5,36 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"slices"
+	"strings"
 )
 
 type Handler func(url *url.URL) error
 
-type viaDispatch struct {
+type viaServer struct {
 	commands []*viaCommand
 }
 
-func (vd *viaDispatch) AddCommand(verb string, handler Handler) *viaCommand {
+func (vs *viaServer) AddCommand(verb string, handler Handler) *viaCommand {
 	vc := &viaCommand{verb: verb, handler: handler}
-	vd.commands = append(vd.commands, vc)
+	vs.commands = append(vs.commands, vc)
 	return vc
 }
 
-func (vd *viaDispatch) Serve(args ...string) error {
+type viaRequest struct {
+	command    *viaCommand
+	parameters []*viaParameter
+	query      map[string][]string
+	err        error
+}
+
+func (vr *viaRequest) ToUrl() *url.URL {
+	return new(url.URL)
+}
+
+type viaParseFunc func(req *viaRequest, args ...string) viaParseFunc
+
+func (vs *viaServer) Serve(args ...string) error {
 
 	// TODO implement default command handling
 
@@ -27,11 +42,13 @@ func (vd *viaDispatch) Serve(args ...string) error {
 		return errors.New("missing args")
 	}
 
-	cmdArg := args[0]
+	req := new(viaRequest)
+	req.query = make(map[string][]string)
 
-	for _, cmd := range vd.commands {
-		if cmd.verb == cmdArg {
-			if u, err := cmd.parse(args...); err == nil {
+	for _, cmd := range vs.commands {
+		if cmd.verb == args[0] {
+			req.command = cmd
+			if u, err := cmd.parse(req, args[1:]...); err == nil {
 				return cmd.handler(u)
 			} else {
 				return err
@@ -39,7 +56,7 @@ func (vd *viaDispatch) Serve(args ...string) error {
 		}
 	}
 
-	return errors.New("not a valid command verb: " + cmdArg)
+	return errors.New("not a valid command verb: " + args[0])
 }
 
 type viaCommand struct {
@@ -48,8 +65,85 @@ type viaCommand struct {
 	parameters []*viaParameter
 }
 
-func (vc *viaCommand) parse(args ...string) (*url.URL, error) {
-	return new(url.URL), nil
+func (vc *viaCommand) parse(req *viaRequest, args ...string) (*url.URL, error) {
+
+	for f := vc.defaultValuesOrParameter(req, args...); f != nil; f = f(req, args...) {
+	}
+
+	return req.ToUrl(), nil
+}
+
+func (vc *viaCommand) defaultValuesOrParameter(req *viaRequest, args ...string) viaParseFunc {
+	if len(args) == 0 {
+		// TODO: check if command actually expects any
+		req.err = errors.New("no args for default values or parameter")
+		return nil
+	}
+
+	switch strings.HasPrefix(args[0], "-") {
+	case false:
+		for _, p := range vc.parameters {
+			if slices.Contains(p.opts, OptDefault) {
+				req.parameters = append(req.parameters, p)
+				if value, err := p.checkValue(args[0]); err == nil {
+					req.query[p.title] = []string{value}
+					return vc.valueOrParameter(req, args[1:]...)
+				} else {
+					req.err = err
+					return nil
+				}
+			}
+		}
+	case true:
+		maybeParamTitle := strings.TrimPrefix(args[0], "-")
+		for _, p := range vc.parameters {
+			if p.title == maybeParamTitle {
+				req.parameters = append(req.parameters, p)
+				req.query[p.title] = nil
+				return vc.valueOrParameter(req, args[1:]...)
+			}
+		}
+		req.err = errors.New("parameter " + args[0] + " not found")
+		return nil
+	}
+
+	return nil
+}
+
+func (vc *viaCommand) valueOrParameter(req *viaRequest, args ...string) viaParseFunc {
+	if len(args) == 0 {
+		return nil
+	}
+
+	switch strings.HasPrefix(args[0], "-") {
+	case false:
+		if len(req.parameters) > 0 {
+			lastParameter := req.parameters[len(req.parameters)-1]
+			if value, err := lastParameter.checkValue(args[0]); err == nil {
+				req.query[lastParameter.title] = append(req.query[lastParameter.title], value)
+				return vc.valueOrParameter(req, args[1:]...)
+			} else {
+				req.err = err
+				return nil
+			}
+		} else {
+			req.err = errors.New("no parameter to set value for")
+			return nil
+		}
+	case true:
+		maybeParamTitle := strings.TrimPrefix(args[0], "-")
+		for _, p := range vc.parameters {
+			if p.title == maybeParamTitle {
+				req.parameters = append(req.parameters, p)
+				req.query[p.title] = nil
+				return vc.valueOrParameter(req, args[1:]...)
+			}
+		}
+		req.err = errors.New("parameter " + maybeParamTitle + " not found")
+		return nil
+	}
+
+	return nil
 }
 
 type viaParameter struct {
@@ -57,6 +151,31 @@ type viaParameter struct {
 	opts         []viaOption
 	values       []string
 	defaultValue string
+}
+
+func (vp *viaParameter) checkValue(maybeValue string) (string, error) {
+	if slices.Contains(vp.opts, OptBoolean) {
+		return "", errors.New("value is a flag")
+	}
+
+	if maybeValue == "" {
+		return "", errors.New("check value not specified")
+	}
+
+	if len(vp.values) == 0 {
+		return maybeValue, nil
+	}
+
+	lcMaybeValue := strings.ToLower(maybeValue)
+
+	for _, v := range vp.values {
+		lcv := strings.ToLower(v)
+		if strings.HasPrefix(lcv, lcMaybeValue) {
+			return v, nil
+		}
+	}
+
+	return "", errors.New("value " + maybeValue + " is not valid for " + vp.title)
 }
 
 type viaOption = int
@@ -93,12 +212,11 @@ func (vp *viaParameter) SetDefaultValue(value string) *viaParameter {
 	return vp
 }
 
-func main() {
+func initViaServer() *viaServer {
+	vs := new(viaServer)
 
-	vd := new(viaDispatch)
-
-	vd.AddCommand("backup", echoHandler)
-	cleanup := vd.AddCommand("cleanup", echoHandler)
+	vs.AddCommand("backup", echoHandler)
+	cleanup := vs.AddCommand("cleanup", echoHandler)
 
 	cleanup.AddParameter("id", OptDefault, OptMultipleValues)
 	cleanup.AddParameter("slug", OptMultipleValues)
@@ -110,7 +228,14 @@ func main() {
 	cleanup.AddParameter("all", OptBoolean)
 	cleanup.AddParameter("test", OptBoolean)
 
-	if err := vd.Serve(os.Args[1:]...); err != nil {
+	return vs
+}
+
+func main() {
+
+	vs := initViaServer()
+
+	if err := vs.Serve(os.Args[1:]...); err != nil {
 		panic(err)
 	}
 
